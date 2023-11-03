28c470130ecdb74d8d157dc0cf2049370c888bc4
yehani <yehanipamunuwa@gmail.com>
2023-11-03 11:58:02 +0530

    Dashboard incomplete

---
+++ src/main/java/lk/ijse/LibraSys/db/Db.java
@@ -0,0 +1,9 @@
+package lk.ijse.LibraSys.db;
+
+public class Db {
+    public static final  String S_NUMBER= "S2001";
+
+    public static final String USER_NAME = "yehani";
+
+    public static final String PW = "Y1234";
+}
