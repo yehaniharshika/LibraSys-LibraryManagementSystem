09e2d64cad5cd1c796b536e1075da53f211a06c1
yehani <yehanipamunuwa@gmail.com>
2023-11-03 18:31:59 +0530

    Dashboard complete

--- src/main/java/lk/ijse/LibraSys/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/LibraSys/controller/DashboardFormController.java
@@ -1,5 +1,33 @@
 package lk.ijse.LibraSys.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardFormController {
 
+    public AnchorPane root;
+
+    public void btnAuthorOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMemberOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReservationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBookOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBookRackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMemberFeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
 }
---
+++ src/main/java/lk/ijse/LibraSys/controller/MemberFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.LibraSys.controller;
+
+public class MemberFormController {
+}
