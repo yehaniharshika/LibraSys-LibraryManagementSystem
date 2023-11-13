e4148183b0787f97e144d83b6a82a4127448bbf1
yehani <yehanipamunuwa@gmail.com>
2023-11-13 15:21:01 +0530

    book rack complete

--- src/main/java/lk/ijse/LibraSys/controller/BookRackFormController.java
+++ src/main/java/lk/ijse/LibraSys/controller/BookRackFormController.java
@@ -1,11 +1,23 @@
 package lk.ijse.LibraSys.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.LibraSys.dto.BookRackDto;
+import lk.ijse.LibraSys.dto.tm.BookRackTm;
+import lk.ijse.LibraSys.model.BookRackModel;
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class BookRackFormController {
 
@@ -22,7 +34,7 @@ public class BookRackFormController {
     private TableColumn<?, ?> colQuantityBooks;
 
     @FXML
-    private TableView<?> tblBookrack;
+    private TableView<BookRackTm> tblBookrack;
 
     @FXML
     private TextField txtCode;
@@ -31,38 +43,145 @@ public class BookRackFormController {
     private TextField txtQuantity;
 
     @FXML
-    private TextField txtnameOfBooks;
+    private TextArea txtNameOfBooks;
+    private BookRackModel bookRackModel = new BookRackModel();
+
+    public void initialize(){
+        loadAllBookRacks();
+        setCellValueFactory();
+    }
 
     @FXML
-    void btnBackOnAction(ActionEvent event) {
-
+    void btnBackOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/dashboard_Form.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage = (Stage) Pane.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Dashboard Form");
+        stage.centerOnScreen();
+        stage.show();
     }
 
     @FXML
     void btnClearOnAction(ActionEvent event) {
+        clearFields();
 
+    }
+
+    private void setCellValueFactory() {
+        colCode.setCellValueFactory(new PropertyValueFactory<>("rackCode"));
+        colQuantityBooks.setCellValueFactory(new PropertyValueFactory<>("qtyBooks"));
+        colNameOfBooks.setCellValueFactory(new PropertyValueFactory<>("nameOfBooks"));
+    }
+
+    private void loadAllBookRacks() {
+        ObservableList<BookRackTm> obList = FXCollections.observableArrayList();
+
+        try {
+            List<BookRackDto> rackList = bookRackModel.getAllBookRack();
+
+            for(BookRackDto dto : rackList){
+                obList.add(
+                        new BookRackTm(
+                            dto.getRackCode(),
+                            dto.getQtyBooks(),
+                            dto.getNameOfBooks()
+
+                ));
+            }
+            tblBookrack.setItems(obList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void clearFields() {
+        txtCode.setText("");
+        txtQuantity.setText("");
+        txtNameOfBooks.setText("");
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        String rackCode = txtCode.getText();
 
+        try {
+            boolean isDeleted = bookRackModel.deleteBookRack(rackCode);
+
+            if(isDeleted){
+                new Alert(Alert.AlertType.INFORMATION,"Deleting Successfully!!!").show();
+                loadAllBookRacks();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"not deleted!!!").show();
+            }
+        } catch (SQLException e) {
+           new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
     }
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
         String rackCode = txtCode.getText();
         int qtyBooks = Integer.parseInt(txtQuantity.getText());
-        String nameOfBooks = txtnameOfBooks.getText();
+        String nameOfBooks = txtNameOfBooks.getText();
+
+        var dto = new BookRackDto(rackCode,qtyBooks,nameOfBooks);
+
+        try {
+            boolean isSaved = bookRackModel.saveBookRack(dto);
+            if(isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"Book Rack Adding Successfully!!!").show();
+                clearFields();
+                loadAllBookRacks();
+                setCellValueFactory();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Book rack Adding failed!!!").show();
+            }
+        } catch (SQLException e) {
+           new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        String rackCode = txtCode.getText();
+        int qtyBooks = Integer.parseInt(txtQuantity.getText());
+        String nameOfBooks = txtNameOfBooks.getText();
 
+        var dto = new BookRackDto(rackCode,qtyBooks,nameOfBooks);
+
+        try {
+            boolean isUpdated = bookRackModel.updateBookRack(dto);
+            if(isUpdated){
+                new Alert(Alert.AlertType.INFORMATION,"Updated successfully!!!").show();
+                clearFields();
+                loadAllBookRacks();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"not updated!!!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
     }
 
     @FXML
     void txtCodeOnAction(ActionEvent event) {
+        String rackCode = txtCode.getText();
 
+        try {
+            BookRackDto dto = bookRackModel.serchBookRack(rackCode);
+
+            if (dto != null){
+                txtCode.setText(dto.getRackCode());
+                txtQuantity.setText(String.valueOf(dto.getQtyBooks()));
+                txtNameOfBooks.setText(dto.getNameOfBooks());
+            }else{
+                new Alert(Alert.AlertType.ERROR,"not found!!!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
     }
 
 }
--- src/main/java/lk/ijse/LibraSys/model/BookRackModel.java
+++ src/main/java/lk/ijse/LibraSys/model/BookRackModel.java
@@ -1,4 +1,81 @@
 package lk.ijse.LibraSys.model;
 
+import lk.ijse.LibraSys.db.DbConnection;
+import lk.ijse.LibraSys.dto.BookRackDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookRackModel {
+    public boolean saveBookRack(BookRackDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO  bookRack VALUES (?,?,?)");
+        pstm.setString(1,dto.getRackCode());
+        pstm.setString(2, String.valueOf(dto.getQtyBooks()));
+        pstm.setString(3, dto.getNameOfBooks());
+
+        boolean isSaved = pstm.executeUpdate() > 0;
+        return isSaved;
+    }
+
+    public  boolean updateBookRack(BookRackDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE bookRack SET qtyBooks=?,nameOfBooks =? WHERE rackCode =?");
+        pstm.setString(1, String.valueOf(dto.getQtyBooks()));
+        pstm.setString(2, dto.getNameOfBooks());
+        pstm.setString(3, dto.getRackCode());
+
+        boolean isUpdated = pstm.executeUpdate() > 0;
+        return isUpdated;
+    }
+
+    public boolean deleteBookRack(String rackCode) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM bookRack WHERE rackCode=?");
+        pstm.setString(1,rackCode);
+
+        boolean isDeleted = pstm.executeUpdate() > 0;
+        return isDeleted;
+    }
+
+    public BookRackDto serchBookRack(String rackCode) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM bookRack WHERE rackCode=?");
+        pstm.setString(1,rackCode);
+
+        ResultSet resultSet = pstm.executeQuery();
+
+        BookRackDto dto = null;
+
+        if(resultSet.next()){
+            dto = new BookRackDto(
+                  resultSet.getString(1),
+                  resultSet.getInt(2),
+                  resultSet.getString(3)
+            );
+        }
+        return dto;
+    }
+
+    public List<BookRackDto> getAllBookRack() throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM bookRack");
+
+        List<BookRackDto> rackList = new ArrayList<>();
+
+        ResultSet resultSet = pstm.executeQuery();
+        while(resultSet.next()){
+            rackList.add(new BookRackDto(
+                resultSet.getString(1),
+                resultSet.getInt(2),
+                resultSet.getString(3)
+            ));
+
+        }
+        return rackList;
+    }
 }
